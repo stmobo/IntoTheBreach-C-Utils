8736ca8337607bbccb38a1a4728e1bef62282f03
Taren <cyril-fahlenbock@hotmail.de>
2018-04-04 23:06:47 +0200

    Add 'ReplaceWeapon' function

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -7,6 +7,12 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <memory>
+
+
+void* offset(void* base, size_t delta) {
+	return (void*)((int)base + delta);
+}
 
 void* get_data(lua_State* L, const char* error, int idx) {
 	void*** userdata = (void***)lua_touserdata(L, idx);
@@ -22,27 +28,54 @@ int getpawnaddr(lua_State* L) {
 	lua_pushinteger(L, (int)pawn);
 	return 1;
 }
-struct Weapon {
-	void* head;
-	void* tail;
-};
-int getweaponname(lua_State* L) {
-	void* weapon_vec_addr = (int*)get_pawn(L, 1)+1;
+typedef std::shared_ptr<void*> Weapon;
 
+std::vector<Weapon>* weapon_vec(void* pawn) {
+	void* weapon_vec_addr = (int*)pawn+1;
+	std::vector<Weapon>* weapon_vector = static_cast<std::vector<Weapon>*>(weapon_vec_addr);
+	return weapon_vector;
+}
+Weapon* weapon_at(void* pawn, size_t idx) {
+	if (idx == 50) {
+		return (Weapon*)offset(pawn, 0x5c);
+	} else {
+		std::vector<Weapon>* weapon_vector = weapon_vec(pawn);
+		if (idx < 0 || weapon_vector->size() <= idx) {
+			return NULL;
+		}
+		return &(*weapon_vector)[idx];
+	}
+}
+int getweaponname(lua_State* L) {
+	void* pawn = get_pawn(L, 1);
 	size_t idx = luaL_checkint(L, 2);
 
-	std::vector<Weapon>* weapon_vector = static_cast<std::vector<Weapon>*>(weapon_vec_addr);
 
-	if (idx >= weapon_vector->size()) {
+	Weapon* weapon = weapon_at(pawn, idx);
+
+	if (weapon == NULL) {
 		lua_pushnil(L);
 		return 1;
 	}
-	void* weapon_addr = (*weapon_vector)[idx].tail;
-	void* string_addr = (void*)((int)weapon_addr + 0xc0);
+	void* string_addr = (void*)((int)weapon->get() + 0xb4);
 
 	std::basic_string<char>* name = static_cast<std::basic_string<char>*>(string_addr);
 	lua_pushstring(L, name->c_str());
 	return 1;
+}
+
+int replace_weapon(lua_State*L) {
+	void* pawn = (int*)get_pawn(L, 1);
+	size_t new_idx = luaL_checkint(L, 2);
+
+	std::vector<Weapon>* weapons = weapon_vec(pawn);
+	Weapon& w = weapons->back();
+
+	Weapon& o = *weapon_at(pawn, new_idx);
+	w.swap(o);
+
+	weapons->pop_back();
+	return 0;
 }
 int* get_pawn_data_at(void* pawn, int offset) {
 	return (int*)((int)pawn + 0x86c + offset);
@@ -65,9 +98,7 @@ int get_max_health(lua_State* L) {
 	return 1;
 }
 
-void* offset(void* base, size_t delta) {
-	return (void*)((int)base + delta);
-}
+
 int get_tile_health(lua_State* L) {
 	void* board = get_data(L, "`board' expected", 1);
 	int x = luaL_checkint(L, 2);
@@ -89,7 +120,7 @@ static const struct luaL_Reg Utils[] = {
 { "SetMaxHealth", set_max_health },
 { "GetMaxHealth", get_max_health },
 { "GetTileHealth", get_tile_health },
-
+{ "ReplaceWeapon", replace_weapon },
 { NULL,           NULL } };
 extern "C" {
 	int  __declspec(dllexport) luaopen_utils(lua_State* L) {
